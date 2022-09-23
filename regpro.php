d57a7a2ce80e05367e1b9301631f4f9d06cb9510
imranaabdullahibala <imranaabdullahibala@gmail.com>
2022-09-23 11:12:06 +0100

    php ext..

--- regpro.php
+++ regpro.php
@@ -104,9 +104,9 @@
 	 	$email = $_POST['email'];
 
 
-	 	 Create connection
+	 	//  Create connection
 	 	$conn = new mysqli($servername, $username, $password, $dbname);
-	 	 Check connection
+	 	//  Check connection
 	 	if ($conn->connect_error) {
 	 	  die("Connection failed: " . $conn->connect_error);
 	 	}
